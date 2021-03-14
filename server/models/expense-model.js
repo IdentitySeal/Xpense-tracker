58b83ea6288a381957d48981292f1f1468845f97
unknown <teemealeheen@gmail.com>
2021-03-14 11:58:09 +0100

    used a callback hook to fix dependency waring in ,useEffect

--- server/models/expense-model.js
+++ server/models/expense-model.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const Expense = mongoose.model( 
     "Expense",
     new mongoose.Schema({
-    description: String,
-    amount:Number
-
-
+        description: String,
+        amount: Number
 })
 .method("toJSON", function() {
     const { __v, _id, ...object } = this.toObject();
